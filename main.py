55189ce8a0ff13be24e9e40a96fcd0a1c46931ed
Shanushekhar <shekharshanu1996@kgpian.iitkgp.ac.in>
2023-08-05 13:58:08 +0530

    updated

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
 
 
-
-if __name__=="__main__":
+def test_logger_and_exception():
      try:
-          training_pipeline_config = config_entity.TrainingPipelineConfig()
-          data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-          print(data_ingestion_config.to_dict())
-          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
-     except Exception as e:
-          print(e)
+          result=3/0
+          print(result)
+     except exception as e:
+          raise e
+     
+if __name__ ==" __main__":
+     try:
+          pass
+
+     except exception as e:
+          print(e)     
