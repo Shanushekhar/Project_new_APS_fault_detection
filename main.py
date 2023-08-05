e05d7dd03fcc4310af6b343c6ef67dda3ba595b2
Shanushekhar <shekharshanu1996@kgpian.iitkgp.ac.in>
2023-08-05 15:00:11 +0530

    created logging module and basic structure

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
-from sensor.utils import get_collection_as_dataframe
 import sys,os
-from sensor.entity import config_entity
-from sensor.components.data_ingestion import DataIngestion
 
 
 def test_logger_and_exception():
      try:
           result=3/0
           print(result)
-     except exception as e:
-          raise e
+     except Exception as e:
+          raise SensorException(e,sys)
      
 if __name__ ==" __main__":
      try:
           pass
 
-     except exception as e:
+     except Exception as e:
           print(e)     
